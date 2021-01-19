a4827c665476d001a9ecd99620ef11c6f4564e9f
dandy <danielsbarbosa27@gmail.com>
2021-01-19 02:43:12 -0300

    Add validando e salvando dados

--- virtualloja_app/lib/models/user.dart
+++ virtualloja_app/lib/models/user.dart
@@ -1,9 +1,11 @@
 class User {
 
-   User({this.email, this.password});
+   User({this.email, this.password, this.name, this.confirmPassword});
 
+  String name;
   String email;
   String password;
+  String confirmPassword;
 
 
 }
--- virtualloja_app/lib/screens/base/signup/signup_screen.dart
+++ virtualloja_app/lib/screens/base/signup/signup_screen.dart
@@ -1,9 +1,18 @@
 import 'package:flutter/material.dart';
+import 'package:virtualloja_app/helpers/validators.dart';
+import 'package:virtualloja_app/models/user.dart';
 
 class SignUpScreen extends StatelessWidget {
+
+  final GlobalKey<FormState> formKey = GlobalKey<FormState>();
+  final GlobalKey<ScaffoldState> scaffoldKey = GlobalKey<ScaffoldState>();
+
+  final User  user = User();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      key: scaffoldKey,
       appBar: AppBar(
         title: const Text('Criar Conta'),
         centerTitle: true,
@@ -11,43 +20,89 @@ class SignUpScreen extends StatelessWidget {
       body: Center(
         child: Card(
           margin: const EdgeInsets.symmetric(horizontal: 16),
-          child: ListView(
-            padding: const EdgeInsets.all(16),
-            shrinkWrap: true,
-            children: [
-              TextFormField(
-                decoration: const InputDecoration(hintText: 'Nome completo'),
-              ),
-          const SizedBox(height: 16,),
-              TextFormField(
-                decoration: const InputDecoration(hintText: 'E-mail'),
-                keyboardType: TextInputType.emailAddress,
-              ),
-          const SizedBox(height: 16,),
-              TextFormField(
-                decoration: const InputDecoration(hintText: 'Senha'),
-                obscureText: true,
-              ),
-          const SizedBox(height: 16,),
-              TextFormField(
-                decoration: const InputDecoration(hintText: 'Repita a Senha'),
-                obscureText: true,
-              ),
-        const SizedBox(height: 16,),
-              SizedBox(
-                height: 44,
-                child: RaisedButton(
-                  color: Theme.of(context).primaryColor,
-                  disabledColor: Theme.of(context).primaryColor.withAlpha(100),
-                  textColor: Colors.white,
-                  onPressed: (){
+          child: Form(
+            key: formKey,
+            child: ListView(
+              padding: const EdgeInsets.all(16),
+              shrinkWrap: true,
+              children: [
+                TextFormField(
+                  decoration: const InputDecoration(hintText: 'Nome completo'),
+                  validator: (name){
+                    if(name.isEmpty)
+                      return 'Campo obrigátorio';
+                    else if(name.trim().split(' ').length <= 1)
+                      return 'Preencha seu Nome Completo';
+                    return null;
+                  },
+                  onSaved: (name) => user.name = name,
+                ),
+            const SizedBox(height: 16,),
+                TextFormField(
+                  decoration: const InputDecoration(hintText: 'E-mail'),
+                  keyboardType: TextInputType.emailAddress,
+                  validator: (email){
+                    if(email.isEmpty)
+                      return 'Campo obrigatório';
+                    else if(!emailValid(email))
+                      return 'E-mail invalid';
+                    return null;
+                  },
+                  onSaved: (email) => user.email = email,
+                ),
+            const SizedBox(height: 16,),
+                TextFormField(
+                  decoration: const InputDecoration(hintText: 'Senha'),
+                  validator: (pass){
+                    if(pass.isEmpty)
+                      return 'Campo obrigatório';
+                    else if(pass.length < 6)
+                      return 'Senha muito curta';
+                    return null;
 
                   },
-                  child: const Text('Criar Conta', style: TextStyle(fontSize: 18),),
+                  onSaved: (pass) => user.password = pass,
+                  obscureText: true,
                 ),
-              )
+            const SizedBox(height: 16,),
+                TextFormField(
+                  validator: (pass){
+                    if(pass.isEmpty)
+                      return 'Campo obrigatório';
+                    else if(pass.length < 6)
+                      return 'Senha muito curta';
+                    return null;
+                  },
+                  onSaved: (pass) => user.confirmPassword = pass,
+                  decoration: const InputDecoration(hintText: 'Repita a Senha'),
+                  obscureText: true,
+                ),
+        const SizedBox(height: 16,),
+                SizedBox(
+                  height: 44,
+                  child: RaisedButton(
+                    color: Theme.of(context).primaryColor,
+                    disabledColor: Theme.of(context).primaryColor.withAlpha(100),
+                    textColor: Colors.white,
+                    onPressed: (){
+                      if(formKey.currentState.validate()){
+                        formKey.currentState.save();
+                        if(user.password != user.confirmPassword){
+                          scaffoldKey.currentState.showSnackBar(SnackBar(
+                            content: const Text('Senhas não coincidem!'),
+                            backgroundColor: Colors.red,
+                          ));
+                          return;
+                        }
 
-            ],
+                      };
+                    },
+                    child: const Text('Criar Conta', style: TextStyle(fontSize: 18),),
+                  ),
+                )
+
+              ],
+            ),
           ),
         ),
       ) ,
