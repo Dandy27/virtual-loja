14002b722330f387e93aed93c63b1eda2e02c6f1
dandy <danielsbarbosa27@gmail.com>
2021-01-22 19:15:37 -0300

    Buscando os tamanhos no firebase

--- virtualloja_app/lib/models/product.dart
+++ virtualloja_app/lib/models/product.dart
@@ -1,18 +1,25 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 
-class Product {
+import 'item_size.dart';
 
-  Product.fromDocument(DocumentSnapshot document){
+class Product {
+  Product.fromDocument(DocumentSnapshot document) {
     id = document.documentID;
     name = document['name'] as String;
     description = document['description'] as String;
     images = List<String>.from(document.data['images'] as List<dynamic>);
+    sizes = (document.data['sizes'] as List<dynamic> ?? []).map(
+            (s) => ItemSize.fromMap(s as Map<String, dynamic>)).toList();
+
+    print(sizes);
 
   }
+
   String id;
   String name;
   String description;
   List<String> images;
+  List<ItemSize> sizes;
 
 
 }
