afffea0f63d8cc48d08cc3915b029316d0da9297
dandy <danielsbarbosa27@gmail.com>
2021-01-21 23:20:33 -0300

    Pesquisando produtos pelo nome

--- virtualloja_app/lib/models/product_manager.dart
+++ virtualloja_app/lib/models/product_manager.dart
@@ -2,7 +2,7 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:virtualloja_app/models/product.dart';
 
-class ProductManager   extends ChangeNotifier{
+class ProductManager extends ChangeNotifier {
   ProductManager() {
     _loadingAllProducts();
   }
@@ -11,9 +11,32 @@ class ProductManager   extends ChangeNotifier{
 
   List<Product> allProducts = [];
 
+  String _search = '';
+
+  String get search => _search;
+
+  set search(String value) {
+    _search = value;
+    notifyListeners();
+  }
+
+  List<Product> get filteredProducts{
+    final List<Product> filteredProducts = [];
+    if (search.isEmpty) {
+      filteredProducts.addAll(allProducts);
+    } else {
+      filteredProducts.addAll(
+          allProducts.where((p) => p.name.toLowerCase().contains(search.
+              toLowerCase()
+      )));
+  }
+
+    return filteredProducts;
+  }
+
   Future<void> _loadingAllProducts() async {
     final QuerySnapshot snapProducts =
-        await firestore.collection('products').getDocuments();
+    await firestore.collection('products').getDocuments();
 
     allProducts =
         snapProducts.documents.map((d) => Product.fromDocument(d)).toList();
