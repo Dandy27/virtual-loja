0b4f879f011d727bce2205efdc7d3a214e11f69c
dandy <danielsbarbosa27@gmail.com>
2021-01-23 20:09:03 -0300

    Salvando o carrinho do usuário e empilhando itens

--- virtualloja_app/lib/models/cart_manager.dart
+++ virtualloja_app/lib/models/cart_manager.dart
@@ -5,32 +5,33 @@ import 'package:virtualloja_app/models/user.dart';
 import 'package:virtualloja_app/models/user_manager.dart';
 
 class CartManager {
-
   List<CartProduct> items = [];
 
   User user;
 
-  void updateUser(UserManager userManager){
+  void updateUser(UserManager userManager) {
     user = userManager.user;
     items.clear();
 
-    if(user != null){
+    if (user != null) {
       _loadCardItem();
     }
-
   }
 
   Future<void> _loadCardItem() async {
     final QuerySnapshot cartSnap = await user.cartReference.getDocuments();
 
     items = cartSnap.documents.map((d) => CartProduct.fromDocument(d)).toList();
-
   }
 
-  void addToCart(Product product){
-    items.add(CartProduct.fromProduct(product));
+  void addToCart(Product product) {
+    try {
+      final e = items.firstWhere((p) => p.stackable(product));
+      e.quantity++;
+    } catch (e) {
+      final cartProduct = CartProduct.fromProduct(product);
+      items.add(cartProduct);
+      user.cartReference.add(cartProduct.toCartItemMap());
+    }
   }
-
 }
-
-
