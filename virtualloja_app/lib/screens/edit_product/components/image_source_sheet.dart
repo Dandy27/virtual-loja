748c3682c1b5a7bd29ef3e0a56b3230445fce89a
dandy <danielsbarbosa27@gmail.com>
2021-01-28 22:58:16 -0300

    Escolhendo a imagem da camera ou da galeria

--- virtualloja_app/lib/screens/edit_product/components/image_source_sheet.dart
+++ virtualloja_app/lib/screens/edit_product/components/image_source_sheet.dart
@@ -2,31 +2,44 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'dart:io';
 
+import 'package:image_picker/image_picker.dart';
+
 class ImageSourceSheet extends StatelessWidget {
+  ImageSourceSheet({this.onImageSelected});
+
+  final Function(File) onImageSelected;
+
+  final ImagePicker picker = ImagePicker();
+
   @override
   Widget build(BuildContext context) {
-    if(Platform.isAndroid)
-    return BottomSheet(
-      onClosing: (){},
-      builder: (_) => Column(
-        mainAxisSize: MainAxisSize.min,
-        crossAxisAlignment: CrossAxisAlignment.stretch,
-        children: [
-          FlatButton(
-            onPressed: (){
+    if (Platform.isAndroid)
+      return BottomSheet(
+        onClosing: () {},
+        builder: (_) => Column(
+          mainAxisSize: MainAxisSize.min,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            FlatButton(
+              onPressed: () async {
+                final PickedFile file =
+                    await picker.getImage(source: ImageSource.camera);
+                onImageSelected(File(file.path));
 
-            },
-            child: const Text('Câmera'),
-          ),
-          FlatButton(
-            onPressed: (){
-
-            },
-            child: const Text('Galeria'),
-          ),
-        ],
-      ),
-    );
+              },
+              child: const Text('Câmera'),
+            ),
+            FlatButton(
+              onPressed: () async {
+                final PickedFile file =
+                    await picker.getImage(source: ImageSource.gallery);
+                onImageSelected(File(file.path));
+              },
+              child: const Text('Galeria'),
+            ),
+          ],
+        ),
+      );
     else
       return CupertinoActionSheet(
         title: const Text('Selecionar foto para o Item'),
@@ -38,15 +51,11 @@ class ImageSourceSheet extends StatelessWidget {
         actions: [
           CupertinoActionSheetAction(
             isDefaultAction: true,
-            onPressed: (){
-
-            },
+            onPressed: () {},
             child: const Text('Câmera'),
           ),
           CupertinoActionSheetAction(
-            onPressed: (){
-
-            },
+            onPressed: () {},
             child: const Text('Galeria'),
           )
         ],
